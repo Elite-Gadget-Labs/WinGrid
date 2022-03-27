43fd2d23cf2140761b82643cae584c3d741a2055
AhmadAli137 <ahmad100307@hotmail.com>
2022-03-27 02:14:51 -0400

    added utility functions for getting nodes from json

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+import os
 from pprint import pprint
 import json
 
 city = "Windsor_Roads.json"
 
-with open(f"./roads/{city}", "r") as f:
-    road_data = json.load(f)
 
-node_clusters = [item["nodes"] for item in list(road_data.values())]
-nodes = [nodes[0] for nodes in node_clusters]
+def get_nodes_from_json(json_path):
+    with open(f"./roads/{json_path}", "r") as f:
+        road_data = json.load(f)
+    node_clusters = [item["nodes"] for item in list(road_data.values())]
+    nodes = [nodes[0] for nodes in node_clusters]
+    return nodes
 
-pprint(nodes)
 
-import numpy as np
-from sklearn.cluster import KMeans
-from scipy.spatial.distance import cdist
-import math
-import matplotlib.pyplot as plt
+def convert_string_nodes_to_float_nodes(string_nodes):
+    return [list(map(float, i)) for i in string_nodes]
 
-# print(np.array(nodes))
+
+def get_all_nodes():
+    nodes = []
+    file_names = os.listdir('./roads')
+    for name in file_names:
+        nodes.extend(get_nodes_from_json(name))
+    return nodes
